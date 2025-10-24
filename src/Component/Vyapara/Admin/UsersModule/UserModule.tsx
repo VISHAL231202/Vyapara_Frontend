aec7c606d7cba20c675e59cdfae2a25613bc9315
Vishal R <vishalfanoffottball@gmail.com>
2025-10-24 14:10:38 +0530

    Login authorization, apicall setup completed

--- src/Component/Vyapara/Admin/UsersModule/UserModule.tsx
+++ src/Component/Vyapara/Admin/UsersModule/UserModule.tsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Breadcrumbs from "../../../../CommonElements/Breadcrumbs/Breadcrumbs";
 import { UserManagement } from "../../../../utils/Constant";
 import { Card, CardBody, Col, Container } from "reactstrap";
 import UsersTab from "./UsersTable/UsersTab";
+import Loader from "../../Loader/Loader";
 
 function UserModule() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate dashboard loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds loader
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Breadcrumbs mainTitle={UserManagement} parent={UserManagement} />
-      <Container fluid>
-        <Card>
-          <CardBody>
-            <Col sm="12">
-              <UsersTab />
-            </Col>
-          </CardBody>
-        </Card>
-      </Container>
+      {loading && <Loader />}
+      <>
+        <Breadcrumbs mainTitle={UserManagement} parent={UserManagement} />
+        <Container fluid>
+          <Card>
+            <CardBody>
+              <Col sm="12">
+                <UsersTab />
+              </Col>
+            </CardBody>
+          </Card>
+        </Container>
+      </>
     </>
   );
 }
